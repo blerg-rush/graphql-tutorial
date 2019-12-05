121969f503d9f4ec80f05fa218ce640c521c9546
TheWillGabriel <will.gabriel@gmail.com>
2019-12-05 07:49:02 -0700

    Associate votes type with user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,6 +2,8 @@ module Types
   class UserType < BaseObject
     field :id, ID, null: false
     field :name, String, null: false
+    field :votes, [VoteType], null: false
+    field :links, [LinkType], null: false
 
     # Totally don't do this in a real project
     field :email, String, null: false
